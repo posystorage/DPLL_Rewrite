2908b5b5c47677f1a4a0df5741a6a8b77b24c4f0
JSY <JSY@DESKTOP-MRF396F.local>
2026-07-08 23:46:12 +0800

    tune blend fll gain low

--- verification/rtl/dpll_integrated_flow_tb.v
+++ verification/rtl/dpll_integrated_flow_tb.v
@@ -201,7 +201,7 @@ module dpll_integrated_flow_tb;
         bus_write(16'h0021, 32'd40000);
         bus_write(16'h0022, 32'd117200);
         bus_write(16'h0023, 32'd8000000);
-        bus_write(16'h0024, 32'd7000000);
+        bus_write(16'h0024, 32'd2000000);
         bus_write(16'h0025, 32'd0);
         bus_write(16'h0026, 32'd40000);
         bus_write(16'h0027, 32'd117200);
